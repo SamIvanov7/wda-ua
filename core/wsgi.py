1b03a22107f410a15482098c8f3bccde47f49d71
SamIvanov7 <sam1206morfey@gmail.com>
2024-09-09 18:45:59 +0200

    update

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,11 +1,6 @@
 import os
-import sys
 
 from django.core.wsgi import get_wsgi_application
-
-# Add the app directory to the Python path
-app_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
-sys.path.append(app_path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.production")
 
